3ca062636b77cd0925abe76b9b7f7b4f7bb009b5
jrburke <jrburke@gmail.com>
2013-06-25 10:45:06 -0700

    Update makefie and build output locations so makefile only runs on file changes

--- apps/email/js/html_cache_restore.js
+++ apps/email/js/html_cache_restore.js
@@ -50,7 +50,7 @@
 
   window.addEventListener('load', function(evt) {
     var scriptNode = document.createElement('script');
-    scriptNode.src = 'js/mail_app_built.js';
+    scriptNode.src = 'built/mail_app.js';
     document.head.appendChild(scriptNode);
   }, false);
 }());
